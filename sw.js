de4bd0fd2db5698a925ead4121f1f326f0295199
dsh-overage <skills2038@gmail.com>
2026-09-25 11:55:52 +0200

    fix: refresh offline cache for guided learning

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE='numen-v1';
-const ASSETS=['./','./index.html','./styles.css','./core.js','./app.js','./manifest.webmanifest'];
+const CACHE='numen-v2';
+const ASSETS=['./','./index.html','./styles.css','./core.js','./lessons.js','./app.js','./manifest.webmanifest'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))));
 self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
